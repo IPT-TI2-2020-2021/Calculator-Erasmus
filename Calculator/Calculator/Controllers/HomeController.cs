c338b3c9669f9027a62f9ba3f8aae5568b8ebd2d
Jose Casimiro Nunes Pereira <casimiro@ipt.pt>
2021-03-25 16:48:26 +0000

    code of server side - process the numbers

--- Calculator/Calculator/Controllers/HomeController.cs
+++ Calculator/Calculator/Controllers/HomeController.cs
@@ -25,16 +25,46 @@ namespace Calculator.Controllers {
       /// <returns></returns>
       [HttpGet]
       public IActionResult Index() {
+
+         ViewBag.Display = "0";
+
          return View();
       }
 
       // 2nd call to our calculator
       [HttpPost]
-      public IActionResult Index( string bt ) {
+      public IActionResult Index(string bt, string display) {
 
+         // process the 'bt' variable, and decide what to do with it
+         switch (bt) {
+            case "1":
+            case "2":
+            case "3":
+            case "4":
+            case "5":
+            case "6":
+            case "7":
+            case "8":
+            case "9":
+            case "0":
+               // if we clik a 'number', we are going to draw the number on the screen
+               if (display == "0") display = bt;
+               else display = display + bt;
+               break;
 
+            case ",":
+               // making the 'display' as double number
+               if (!display.Contains(',')) display = display + ',';
+               break;
 
+            case "+/-":
 
+               break;
+
+         } // switch (bt)
+
+         // send some information to the View
+         ViewBag.Display = display;
 
          return View();
       }
